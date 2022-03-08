fb99e443881336218a52b06f0e5d6a61d2a32ca6
thorio <thorio.git@gmail.com>
2022-03-08 18:33:40 +0100

    Implement calculator config

--- gravel-provider-calculator/src/lib.rs
+++ gravel-provider-calculator/src/lib.rs
@@ -6,22 +6,27 @@
 //! Selecting the hit copies the calculated value to the system's clipboard.
 
 use clipboard::{ClipboardContext, ClipboardProvider};
-use gravel_core::{plugin::*, scoring::MAX_SCORE, *};
+use gravel_core::{config::PluginConfigAdapter, plugin::*, scoring::MAX_SCORE, *};
 use meval::eval_str;
+use serde::Deserialize;
 use std::sync::mpsc::Sender;
 
+const DEFAULT_CONFIG: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/config.yml"));
+
 pub fn register_plugins(registry: &mut PluginRegistry) {
-	let definition = PluginDefinition::new("calculator").with_provider(|_config| Box::new(CalculatorProvider::new()));
+	let definition = PluginDefinition::new("calculator").with_provider(get_provider);
 
 	registry.register(definition);
 }
 
-struct CalculatorProvider {}
+fn get_provider(config: &PluginConfigAdapter) -> Box<dyn Provider> {
+	let plugin_config = config.get::<Config>(DEFAULT_CONFIG);
 
-impl CalculatorProvider {
-	pub fn new() -> Self {
-		CalculatorProvider {}
-	}
+	Box::new(CalculatorProvider { config: plugin_config })
+}
+
+struct CalculatorProvider {
+	pub config: Config,
 }
 
 impl Provider for CalculatorProvider {
@@ -29,16 +34,16 @@ impl Provider for CalculatorProvider {
 		let result = eval_str(query);
 
 		match result {
-			Ok(result) => QueryResult::single(get_hit(result)),
+			Ok(result) => QueryResult::single(get_hit(&self.config, result)),
 			Err(_err) => QueryResult::empty(),
 		}
 	}
 }
 
-fn get_hit(result: f64) -> Box<dyn Hit> {
+fn get_hit(config: &Config, result: f64) -> Box<dyn Hit> {
 	let title = round(result, 15).to_string();
 
-	let hitdata = HitData::new(&title, "Copy to clipboard").with_score(MAX_SCORE);
+	let hitdata = HitData::new(&title, &config.subtitle).with_score(MAX_SCORE);
 
 	Box::new(SimpleHit::new(hitdata, set_clipboard))
 }
@@ -53,4 +58,9 @@ fn set_clipboard(hit: &SimpleHit<()>, sender: &Sender<FrontendMessage>) {
 	ctx.set_contents(hit.get_data().title.clone()).unwrap();
 
 	sender.send(FrontendMessage::Hide).unwrap();
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+	pub subtitle: String,
 }
