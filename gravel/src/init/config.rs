02a9a80d3a6788c97d398e748b13ec9cda3a499f
thorio <thorio.git@gmail.com>
2023-09-13 22:38:03 +0200

    Improve config

--- gravel/src/init/config.rs
+++ gravel/src/init/config.rs
@@ -1,18 +1,15 @@
 use ::config::{builder::DefaultState, Config, ConfigBuilder, File, FileFormat};
 use gravel_core::config::ConfigManager;
+use std::{env, path::PathBuf};
 
 const DEFAULT_CONFIG: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/config.yml"));
 
-#[cfg(target_os = "linux")]
-const CONFIG_PATH: &str = "${XDG_CONFIG_HOME:-$HOME/.config}/gravel";
-#[cfg(windows)]
-const CONFIG_PATH: &str = "${XDG_CONFIG_HOME:-$USERPROFILE/.config}/gravel";
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const ENV_GRAVEL_CONFIG_PATH: &str = "GRAVEL_CONFIG_PATH";
 
 /// Reads and deserializes the configuration from multiple sources:
 /// - baked-in default config (config.yml in crate root)
 /// - user config file in `$XDG_CONFIG_HOME/gravel/user.yml`
-/// - dev config file in `$XDG_CONFIG_HOME/gravel/dev.yml`, only when
-///   compiling in debug mode.
 ///
 /// Each layer can override the values of the previous layers.
 pub fn config() -> ConfigManager {
@@ -24,21 +21,45 @@ pub fn config() -> ConfigManager {
 
 /// Initializes up the [`ConfigBuilder`] with all sources.
 fn get_builder() -> ConfigBuilder<DefaultState> {
-	let config_path = shellexpand::env(CONFIG_PATH).expect("used variables should always be present on all system");
-	let user_config_path = format!("{}/user.yml", config_path);
-
-	#[allow(unused_mut)]
-	let mut builder = Config::builder()
+	Config::builder()
 		.add_source(File::from_str(DEFAULT_CONFIG, FileFormat::Yaml))
-		.add_source(File::with_name(&user_config_path).required(false));
+		.add_source(File::from(get_user_config_path()).required(false))
+}
 
-	// dev-only config layer, for example to use a different hotkey for the dev instance
-	#[cfg(debug_assertions)]
-	{
-		let dev_config_path = format!("{}/dev.yml", config_path);
+fn get_user_config_path() -> PathBuf {
+	get_user_config_dir().join("config.yml")
+}
 
-		builder = builder.add_source(File::with_name(&dev_config_path).required(false));
+fn get_user_config_dir() -> PathBuf {
+	if let Ok(path) = env::var(ENV_GRAVEL_CONFIG_PATH) {
+		return path.into();
 	}
 
-	builder
+	get_xdg_config_home().join(APP_NAME)
+}
+
+fn get_xdg_config_home() -> PathBuf {
+	if let Ok(path) = env::var("XDG_CONFIG_HOME") {
+		return path.into();
+	}
+
+	#[cfg(target_os = "linux")]
+	return get_home().join(".config");
+
+	#[cfg(windows)]
+	return get_appdata_roaming();
+}
+
+#[cfg(target_os = "linux")]
+fn get_home() -> PathBuf {
+	let home = env::var("HOME").expect("$HOME is undefined");
+
+	PathBuf::from(home)
+}
+
+#[cfg(windows)]
+fn get_appdata_roaming() -> PathBuf {
+	let roaming = env::var("APPDATA").expect("$APPDATA is undefined");
+
+	PathBuf::from(roaming)
 }
