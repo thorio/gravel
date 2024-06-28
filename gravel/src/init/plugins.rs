d8628105824ac6cf87468c2699bc44be34b1eb52
thorou <thorou@chirality.de>
2024-06-28 22:15:47 +0200

    Make builtins optional

--- gravel/src/init/plugins.rs
+++ gravel/src/init/plugins.rs
@@ -15,14 +15,22 @@ fn register_externals(_registry: &mut PluginRegistry) {
 	// TODO: load external plugins
 }
 
-/// Registers plugins directly compiled into the binary.
+/// Registers plugins compiled directly into the binary.
+#[allow(unused_variables)]
 fn register_builtins(registry: &mut PluginRegistry) {
+	#[cfg(feature = "fltk")]
 	registry.register(gravel_frontend_fltk::get_plugin());
 
+	#[cfg(feature = "calculator")]
 	registry.register(gravel_provider_calculator::get_plugin());
+	#[cfg(feature = "exec")]
 	registry.register(gravel_provider_exec::get_plugin());
+	#[cfg(feature = "kill")]
 	registry.register(gravel_provider_kill::get_plugin());
+	#[cfg(feature = "program")]
 	registry.register(gravel_provider_program::get_plugin());
+	#[cfg(feature = "system")]
 	registry.register(gravel_provider_system::get_plugin());
+	#[cfg(feature = "websearch")]
 	registry.register(gravel_provider_websearch::get_plugin());
 }
