b33c3fabd253c7c8beaf0c63fd20c73961ebfb26
thorou <thorou@chirality.de>
2024-07-22 22:49:03 +0200

    WIP

--- gravel/src/init/cli.rs
+++ gravel/src/init/cli.rs
@@ -1,6 +1,7 @@
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use clap_verbosity_flag::{InfoLevel, Verbosity};
-use std::path::PathBuf;
+use std::{error::Error, path::PathBuf};
 
 pub fn cli() -> Args {
 	Args::parse()
@@ -12,6 +13,10 @@ pub fn cli() -> Args {
 pub struct Args {
 	#[command(flatten)]
 	pub logging: LogArgs,
+
+	/// Key.subkey=value pairs for dynamically patching the config
+	#[clap(short, long, value_delimiter = ',', value_parser = parse_key_val::<String, String>)]
+	pub config_override: Vec<(String, String)>,
 }
 
 #[derive(clap::Args, Debug)]
@@ -26,4 +31,19 @@ pub struct LogArgs {
 	/// Defaults to "$XDG_STATE_DIR/gravel/current.log"; Set to "off" to disable
 	#[arg(long)]
 	pub log_file: Option<PathBuf>,
+}
+
+/// Parse a single key-value pair
+fn parse_key_val<T, U>(s: &str) -> Result<(T, U)>
+where
+	T: std::str::FromStr,
+	T::Err: Error + Send + Sync + 'static,
+	U: std::str::FromStr,
+	U::Err: Error + Send + Sync + 'static,
+{
+	let (key, value) = s
+		.split_once('=')
+		.ok_or_else(|| anyhow!("invalid KEY=value: no `=` found in `{s}`"))?;
+
+	Ok((key.parse()?, value.parse()?))
 }
